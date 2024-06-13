5dd8d08479cbd4e7a0c3fa23b45b722ce6200477
Matheus <147951308+mparreiracruz@users.noreply.github.com>
2024-06-13 16:46:18 -0300

    Create Aula_03_arquivo1.py

---
+++ Aula_03_arquivo1.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+vetor = [1, 5, 9]
+
+print(type(vetor))
+
+meuvetor = np.array(vetor)
+
+print(meuvetor)
+
+print(type(meuvetor))
+
+print(np.arange(1, 20, 2))
+
+print(np.zeros(shape=(4, 4)))
+
+print(np.ones(shape=(6, 5)))
+
+print(type(0))
+
+print(type(0.))
+
