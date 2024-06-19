9b995543e82a683b7be1566609032326d1e6c2a7
Matheus <147951308+mparreiracruz@users.noreply.github.com>
2024-06-19 12:14:07 -0300

    Update Aula_08_arquivo2.py

--- Aula_08_arquivo2.py
+++ Aula_08_arquivo2.py
@@ -1,1 +1,27 @@
+import numpy as np
 
+import cv2
+
+#Variáveis
+#Desenho é falso quando o clique não estiver acionado
+#Desenho é verdadeiro quando o clique estiver pressionado
+desenho = False
+
+ix = -1
+iy = -1
+
+
+#Função
+
+
+
+#Apresentação
+
+imagem = np.zeros((400, 400, 3))
+
+while True:
+    cv2.imshow('minha_tela', imagem)
+    if cv2. waitKey(20) & 0xFF == 27:
+        break
+
+cv2.destroyAllWindows()
