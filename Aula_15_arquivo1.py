6e883aaa92bf11b4da8c8ab7ad8359b33d63a214
Matheus <147951308+mparreiracruz@users.noreply.github.com>
2024-06-25 16:25:37 -0300

    Update Aula_15_arquivo1.py

--- Aula_15_arquivo1.py
+++ Aula_15_arquivo1.py
@@ -8,29 +8,31 @@ def apresentaImg(imagem):
     ax.imshow(imagem, cmap='gray')
     plt.show()
 
-leite_condensado = cv2.imread('leite_condensado.jpeg')
-leite_condensado = cv2.cvtColor(leite_condensado, cv2.COLOR_BGR2GRAY)
 
-apresentaImg(leite_condensado)
+leite_condensado = cv2.imread('zuzu_lapte_zoom.jpeg')
+leite_condensado = cv2.cvtColor(leite_condensado, cv2.COLOR_BGR2RGB)
+
+# apresentaImg(leite_condensado)
 
 pratileira_leite = cv2.imread('prateleira_de_leite.jpeg')
-pratileira_leite = cv2.cvtColor(pratileira_leite, cv2.COLOR_RGB2GRAY)
-apresentaImg(pratileira_leite)
+pratileira_leite = cv2.cvtColor(pratileira_leite, cv2.COLOR_RGB2BGR)
+# apresentaImg(pratileira_leite)
 
 # Detecção de força bruta
 # objeto de detecção!
-orb = cv2.ORB_create()
 
+orb = cv2.ORB.create()
 #Retorna os pontos chaves e destinos
-kpt1, des1 = orb.detectAndCompute(leite_condensado, None)
-kpt2, des2 = orb.detectAndCompute(leite_condensado, None)
+kp1, des1 = orb.detectAndCompute(leite_condensado, None)
+kp2, des2 = orb.detectAndCompute(pratileira_leite, None)
 
 forca_bruta = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 matches = forca_bruta.match(des1, des2)
 
-print(matches[20].distance)
+print(matches[1].distance)
 
+matches = sorted(matches, key=lambda x:x.distance)
 
+leite_matches = cv2.drawMatches(leite_condensado, kp1, pratileira_leite, kp2, matches[:10], None, flags=2)
 
-
-
+apresentaImg(leite_matches)
