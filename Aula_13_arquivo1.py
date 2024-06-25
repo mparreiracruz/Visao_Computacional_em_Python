d0009d113d6eee26b6f6110283ee26c3458685e4
Matheus <147951308+mparreiracruz@users.noreply.github.com>
2024-06-25 12:13:15 -0300

    Update Aula_13_arquivo1.py

--- Aula_13_arquivo1.py
+++ Aula_13_arquivo1.py
@@ -21,8 +21,8 @@ xadrez_real = cv2.cvtColor(xadrez_real, cv2.COLOR_BGR2RGB)
 # plt.show()
 
 cinza_xadrez_real = cv2.cvtColor(xadrez_real, cv2.COLOR_RGB2GRAY)
-plt.imshow(cinza_xadrez_real, cmap='gray')
-plt.show()
+# plt.imshow(cinza_xadrez_real, cmap='gray')
+# plt.show()
 
 # print(cinza_xadrez_real)
 
@@ -30,10 +30,31 @@ cinza = np.float32(cinza_xadrez)
 
 print(cinza)
 # Aplicando o algoritmo Harris Corner Detection
-destino = cv2.cornerHarris(src= cinza_xadrez, blockSize=2, ksize=3, k=0.04)
+destino = cv2.cornerHarris(src=cinza_xadrez, blockSize=20, ksize=3, k=0.04)
 
 # Transformação morfológica pelo cv2.dilate()
+destino = cv2.dilate(destino, None)
 
-plt.show(destino)
+xadrez[destino > 0.01 * destino.max()] = [0, 255, 0]
 
-# parei em 15:34
+print(destino.max())
+
+print(destino)
+
+plt.imshow(xadrez)
+
+plt.show()
+
+cinza = np.float32(cinza_xadrez_real)
+
+print(cinza)
+
+destino = cv2.cornerHarris(src=cinza_xadrez_real, blockSize=3, ksize=3, k=0.04)
+
+destino = cv2.dilate(destino, None)
+
+xadrez_real[destino > 0.01 * destino.max()] = [255, 255, 0]
+
+plt.imshow(xadrez_real)
+
+plt.show()
